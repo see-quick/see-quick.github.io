d9b84091f8e00e812d9f4546356b85e9f53b2b48
see-quick <maros.orsak159@gmail.com>
2025-03-30 14:59:40 +0200

    correct
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- _plugins/quint_block.rb
+++ _plugins/quint_block.rb
@@ -1,31 +1,23 @@
-# frozen_string_literal: true
-
 require 'rouge'
 
 module Jekyll
-  # Custom Liquid block tag for `{% quint %}` to highlight Quint code
   class QuintBlock < Liquid::Block
     def render(context)
       code = super.strip
 
-      # Use the custom lexer and legacy HTML formatter for Rouge
       formatter = Rouge::Formatters::HTMLLegacy.new
       lexer = Rouge::Lexers::Quint.new
-
-      # Highlight the code with Rouge
       highlighted = formatter.format(lexer.lex(code))
 
-      # Wrap the result in a styled container for language-quint
       <<~HTML
-        <div class="language-quint highlighter-rouge">
-          <div class="highlight">
-            <pre class="highlight"><code class="language-quint">#{highlighted}</code></pre>
-          </div>
+      <div class="highlight language-quint">
+        <div class="highlight">
+          <pre><code class="language-quint">#{highlighted}</code></pre>
         </div>
+      </div>
       HTML
     end
   end
 end
 
-# Register the block tag with Liquid so you can write '```quint'  and  '```'` in markdown
 Liquid::Template.register_tag('quint', Jekyll::QuintBlock)
--- _plugins/rouge_lexer_quint.rb
+++ _plugins/rouge_lexer_quint.rb
@@ -1,18 +1,13 @@
-# frozen_string_literal: true
-
 require 'rouge'
 
 module Rouge
   module Lexers
-    # A custom Rouge lexer for the Quint specification language
-    # See: https://github.com/informalsystems/quint
     class Quint < RegexLexer
       title "Quint"
       desc "Quint specification language"
       tag 'quint'
       filenames '*.qnt'
 
-      # Define all Quint keywords like `val`, `action`, etc.
       def self.keywords
         @keywords ||= Set.new %w(
           module val var def action temporal pure nondet
@@ -20,20 +15,18 @@ module Rouge
         )
       end
 
-      # Define primitive and composite types
       def self.types
         @types ||= Set.new %w(
           int bool str Set List Rec Tup
         )
       end
 
-      # Main lexer state
       state :root do
-        rule %r{//.*$}, Comment::Single                     # Single-line comment
-        rule %r/[-+]?[0-9]+/, Num::Integer                  # Numbers like -1, 0, 42
-        rule %r/"/, Str, :string                           # Begin string literal
-        rule %r/[(),{}]/, Punctuation                      # Brackets and braces
-        rule %r/[a-zA-Z_][a-zA-Z0-9_]*/ do |m|             # Identifiers
+        rule %r{//.*$}, Comment::Single
+        rule %r/[-+]?[0-9]+/, Num::Integer
+        rule %r/"/, Str, :string
+        rule %r/[(),{}]/, Punctuation
+        rule %r/[a-zA-Z_][a-zA-Z0-9_]*/ do |m|
           if self.class.keywords.include? m[0]
             token Keyword
           elsif self.class.types.include? m[0]
@@ -42,15 +35,14 @@ module Rouge
             token Name
           end
         end
-        rule %r/[:=]/, Operator                             # Assignment and typing
-        rule %r/'/, Name::Decorator                        # Prime (') notation
-        rule %r/_\d+/, Name::Variable                      # Tuple access like _1, _2
-        rule %r/\./, Operator                              # Dot operator (e.g., heldBy._1)
-        rule %r/[,;]/, Punctuation                         # Separators
-        rule %r/\s+/, Text                                 # Whitespace
+        rule %r/[:=]/, Operator
+        rule %r/'/, Name::Decorator  # or another fitting class
+        rule %r/_\d+/, Name::Variable
+        rule %r/\./, Operator
+        rule %r/[,;]/, Punctuation
+        rule %r/\s+/, Text
       end
 
-      # String literals (e.g., "some text")
       state :string do
         rule %r/"/, Str, :pop!
         rule %r/[^"]+/, Str
